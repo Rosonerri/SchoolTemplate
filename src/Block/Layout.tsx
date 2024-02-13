e380a35479fc31911623c944fa74491b82320596
Ajayi Tobi Gabriel <gabrieldotajayi4real@gmail.com>
2024-02-13 13:46:51 +0100

    Footer updated

--- src/Block/Layout.tsx
+++ src/Block/Layout.tsx
@@ -2,9 +2,10 @@ import Header from "./Header";
 import { Outlet } from "react-router-dom";
 import WelcomePage from "../Pages/WelcomePage";
 import Teachers from "../Pages/Teachers";
-import Footer from "../Pages/Footer"
+import Footer from "../Pages/Footer";
 import Footer2 from "../Pages/Footer2";
 import Scroll from "./Scroll";
+import Contact from "../Pages/Contact";
 const Layout = () => {
   return (
     <div>
@@ -12,9 +13,9 @@ const Layout = () => {
       <Outlet />
       <WelcomePage />
       <Teachers />
-      <Footer/>
-      <Scroll/>
-      <Footer2/>
+      <Contact />
+      <Scroll />
+      <Footer2 />
     </div>
   );
 };
--- src/Pages/Footer2.tsx
+++ src/Pages/Footer2.tsx
@@ -1,33 +1,86 @@
-import { FaFacebook } from "react-icons/fa";
-import { FaTwitter } from "react-icons/fa";
-import { FaSquareInstagram } from "react-icons/fa6";
-const Footer = () => {
+import logo from "../assets/logo.jpg";
+import fb from "../assets/fb.png";
+import ig from "../assets/ig.png";
+import twitter from "../assets/twitter.png";
+import linkden from "../assets/linkden.png";
+
+const Footer2 = () => {
   return (
-    <div className="w-full m-h-[60vh]  flex justify-center items-center border-b mt-[20px]">
-      <div className="w-[90%] h-full bg-white flex justify-around items-center flex-col">
-        <div className="text-center grid grid-cols-2 gap-9 md:grid-cols-2 xl:grid-cols-2">
-          <div className="rounded-[10px] h-[30vh] w-[400px] p-6">
-            <div className="mb-[15px] flex"></div>
-            <div className="font-[400]">
-              <p>CopyWrite, All Right Reserved</p>
+    <div className="w-full flex justify-center items-center">
+      <div className="w-[85%] py-[20px] text-white grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4">
+        <div className="flex justify-center items-center">
+          <div className="p-[20px] flex justify-center items-center flex-col gap-3">
+            <img
+              src={logo}
+              alt=""
+              className="w-[150px] h-[100px] object-contain"
+            />
+            <div className="text-center text-blue-950">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+              libero, placeat aliquam.
+            </div>
+            <div className="flex">
+              <div className="py-1 px-2 rounded-full hover:cursor-pointer hover:hoverbounce">
+                <img
+                  src={fb}
+                  alt=""
+                  className="w-[35px] h-[35px] object-cover"
+                />
+              </div>
+              <div className="py-1 px-2 rounded-full hover:cursor-pointer hover:hoverbounce">
+                <img
+                  src={ig}
+                  alt=""
+                  className="w-[35px] h-[35px] object-cover"
+                />
+              </div>
+              <div className="py-1 px-2 rounded-full hover:cursor-pointer hover:hoverbounce">
+                <img
+                  src={twitter}
+                  alt=""
+                  className="w-[35px] h-[35px] object-cover  "
+                />
+              </div>
+              <div className="py-1 px-2 rounded-full hover:cursor-pointer hover:hoverbounce">
+                <img
+                  src={linkden}
+                  alt=""
+                  className="w-[35px] h-[35px] object-cover "
+                />
+              </div>
             </div>
           </div>
-          <div className="rounded-[10px] h-[30vh] p-6 flex ml-[30px]">
-            <div className="mb-[15px] flex">
-              <div className="mr-[10px] w-[50px] h-[50px] rounded-[50%] shadow-md bg-blue-900 text-[white] font-[700] flex justify-center items-center">
-                <FaFacebook className="text-white text-[20px]" />
-              </div>
-            </div>
-            <div className="mb-[15px] flex">
-              <div className="mr-[10px] w-[50px] h-[50px] rounded-[50%] shadow-md bg-blue-900 text-[white] font-[700] flex justify-center items-center">
-                <FaTwitter className="text-white text-[20px]" />
-              </div>
-            </div>
-            <div className="mb-[15px] flex">
-              <div className="mr-[10px] w-[50px] h-[50px] rounded-[50%] shadow-md bg-blue-900 text-[white] font-[700] flex justify-center items-center">
-                <FaSquareInstagram className="text-white text-[20px]" />
-              </div>
-            </div>
+        </div>
+        <div className="py-[20px] col-span-3 grid grid-cols-2 md:grid-cols-3">
+          <div className="h-full p-[30px] text-blue-950">
+            <div className="mb-[10px] font-semibold text-[22px]">Assets</div>
+            <ul>
+              <li>consectetur</li>
+              <li>ipsum</li>
+              <li>laudantium</li>
+              <li>nostrum</li>
+              <li>eligendi</li>
+            </ul>
+          </div>
+          <div className="h-full p-[30px] text-blue-950">
+            <div className="mb-[10px] font-semibold text-[22px]">Resources</div>
+            <ul>
+              <li>consectetur</li>
+              <li>ipsum</li>
+              <li>laudantium</li>
+              <li>nostrum</li>
+              <li>eligendi</li>
+            </ul>
+          </div>
+          <div className="h-full p-[30px] text-blue-950">
+            <div className="mb-[10px] font-semibold text-[22px]">Research</div>
+            <ul>
+              <li>consectetur</li>
+              <li>ipsum</li>
+              <li>laudantium</li>
+              <li>nostrum</li>
+              <li>eligendi</li>
+            </ul>
           </div>
         </div>
       </div>
@@ -35,4 +88,4 @@ const Footer = () => {
   );
 };
 
-export default Footer;
+export default Footer2;
